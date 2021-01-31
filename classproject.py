2e93fdd3672f5b29e70506a2734ffb0d599b7f86
BlownCapacitor <52745475+BlownCapacitor@users.noreply.github.com>
2021-01-31 09:51:02 +0530

    Update classproject.py

--- classproject.py
+++ classproject.py
@@ -3,10 +3,11 @@ import glob
 import shutil
 import os
 
-src_dir = "/Users/swatiahuja/Desktop"
+
+src_dir = "/Users/swatiahuja/Desktop" 
 dst_dir = "/Users/swatiahuja/Documents"
-for jpegfile in glob.iglob(os.path.join(src_dir, "*.jpeg")):
+for jpegfile in glob.iglob(os.path.join(src_dir, "*.jpeg")): #moves jpeg files
     shutil.copy(jpegfile, dst_dir)
-os.system("open \"\" https://code.whitehatjr.com/s/dashboard")
-os.system("open \"\" https://mail.google.com")
-os.system("open \"\" https://github.com/")
+os.system("open \"\" https://code.whitehatjr.com/s/dashboard")#opens most used websites
+os.system("open \"\" https://mail.google.com")#opens most used websites
+os.system("open \"\" https://github.com/")#opens most used websites
